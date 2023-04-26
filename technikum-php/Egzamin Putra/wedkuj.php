a1ddea9365ff66f35254b06acb2dc59e1d9126e1
Adrian Rigga <riggaadrian312@gmail.com>
2023-04-26 10:46:24 +0200

    Zadanie Egzaminacyjne

---
+++ technikum-php/Egzamin Putra/wedkuj.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css" />
+</head>
+<body>
+    <header> Portal dla wędkarzy </header>
+    
+</body>
+</html>
